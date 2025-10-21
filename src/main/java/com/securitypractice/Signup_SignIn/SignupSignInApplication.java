ba1b73d533e4abdc0d102da1c19f0fa1e8863116
vikas <vikasreddy061@gmail.com>
2025-10-21 10:23:03 -0400

    Adding Springboot scaffold

---
+++ src/main/java/com/securitypractice/Signup_SignIn/SignupSignInApplication.java
@@ -0,0 +1,13 @@
+package com.securitypractice.Signup_SignIn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SignupSignInApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SignupSignInApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/securitypractice/Signup_SignIn/SignupSignInApplicationTests.java
@@ -0,0 +1,13 @@
+package com.securitypractice.Signup_SignIn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SignupSignInApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
